0c630992ad49298c158566bb43105da97fcf7a7a
Aaron Olson <airolson@gmail.com>
2015-04-05 14:10:44 -0400

    Chapter 8 WIP known-city-edges

--- wumpus.lisp
+++ wumpus.lisp
@@ -132,3 +132,26 @@
 (defun draw-city ()
   (ugraph->png "city.dot" *congestion-city-nodes* *congestion-city-edges*))
 
+(defun known-city-nodes ()
+  (mapcar (lambda (node)
+	    (if (member node *visited-nodes*)
+		(let ((n (assoc node *congestion-city-nodes*)))
+		  (if (eql node *player-pos*)
+		      (append n '(*))
+		      n))
+		(list node '?)))
+	  (remove-duplicates
+	   (append *visited-nodes*
+		   (mapcan (lambda (node)
+			     (mapcar #'car
+				     (cdr (assoc node *congestion-city-edges*))))
+			   *visited-nodes*)))))
+
+(defun known-city-edges ()
+  (mapcar (lambda (node)
+	    (cons node (mapcar (lambda (x)
+				 (if (member (car x) *visited-nodes*)
+				     x
+				     (list (car x))))
+			       (cdr (assoc node *congestion-city-edges*)))))
+	  *visited-nodes*))
